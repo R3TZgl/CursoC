e9b6c7e2d69612f5b9ddc0ea399604615d8258ed
R3TZgl <gretzlaff13@gmail.com>
2022-06-15 22:06:55 -0300

    js

--- HTML e CSS/java/novo.js
+++ HTML e CSS/java/novo.js
@@ -4,7 +4,7 @@ function novo(){
 }
 
 
-function iniciar(){
+const iniciar = () => {
     var numero1 = Number(document.getElementById("num1").value)
     var numero2 = Number(document.getElementById("num2").value)
 
