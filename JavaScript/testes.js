78ffe6e29ed96315f9018276187e17b18e96fe11
Gabriel Retzlaff <93136568+R3TZgl@users.noreply.github.com>
2022-08-02 20:04:41 -0300

    teste

---
+++ JavaScript/testes.js
@@ -0,0 +1,25 @@
+const calculadora = {
+    duplicar: duplicar = (numero) => {
+        return numero * 2
+    },
+    somar: somar = (num1, num2) => {
+        return num1 + num2
+    }
+}
+
+const botaoVezesDois = true;
+const botaoSomar = true;
+let dobro = 0;
+let soma = 0;
+
+if (botaoVezesDois){
+    dobro = calculadora["duplicar"];
+}
+if (botaoSomar){
+    soma = calculadora["somar"];
+}
+
+console.log(dobro(5));
+console.log(soma(5, 3));
+
+
