95c07809a5f9830e958ce77e0b5a91db90305c73
Gabriel Retzlaff <gretzlaff13@gmail.com>
2022-08-03 22:27:03 -0300

    sim

--- JavaScript/projeto/assets/js/api_github.js
+++ JavaScript/projeto/assets/js/api_github.js
@@ -1,4 +1,7 @@
-let produtos = [];
+const headers = new Headers();
+headers.append("Authorization", "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");
+
+
 
 const renderiza_foto = (foto) =>{
     const img_foto = document.getElementById("foto");
@@ -6,14 +9,51 @@ const renderiza_foto = (foto) =>{
 }
 
 const renderiza_nome = (nome) => {
-    const nick = document.getElementById()    
+    const nick = document.getElementById("nome");
+    nick.innerText = `${nome}`;    
 }
 
-fetch('https://api.github.com/users/R3TZgl')
+const renderiza_link = (html_url) => {
+    const link = document.getElementById("linkGitHub");
+    link.href=`${html_url}`;
+}
+
+const upar_bio = (dataBio) => {
+    const bio = document.getElementById("bio");
+    bio.innerText = `${dataBio}`;
+}
+
+const upar_repositorio = (data) => {
+    const repositorio = document.getElementById("repoName");
+    repositorio.innerText = `${data.name}`;
+    repositorio.href = `${data.html_url}`;
+}
+
+
+fetch('https://api.github.com/users/R3TZgl', {headers: headers})
     .then((response => response.json()))
     .then(data => {
         console.log(data);
         renderiza_foto(data.avatar_url)
+        renderiza_nome(data.name)
+        renderiza_link(data.html_url)
+        upar_bio(data.bio)
+    })
+    .catch( error => {  // Para status de erro
+        console.error("Algo deu errado na requisição", error)}
+    )
+
+    .finally((finalizar) => {
+        console.warn("sempre cai aqui")
+    })
+
+
+fetch("https://api.github.com/users/R3TZgl/repos", {headers: headers})
+    .then((response => response.json()))
+    .then(data => {
+        console.log(data);
+        data.foreach(upar_repositorio)
+        
     })
     .catch( error => {  // Para status de erro
         console.error("Algo deu errado na requisição", error)}
